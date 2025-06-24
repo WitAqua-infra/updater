6e06acb894ec3a79b2a60d6d0a9f38d1e978598d
soralis0912 <soralis.nem@gmail.com>
2025-06-25 01:32:28 +0900

    fix code

--- gen_mirror_json.py
+++ gen_mirror_json.py
@@ -19,8 +19,14 @@ builds = {}
 for f in [os.path.join(dp, f) for dp, dn, fn in os.walk(FILE_BASE) for f in fn]:
     data = open(f)
     filename = f.split('/')[-1]
-    # lineage-14.1-20171129-nightly-hiaeul-signed.zip
-    _, version, builddate, buildtype, device = os.path.splitext(filename)[0].split('-')
+    parts = os.path.splitext(filename)[0].split('-')
+    if len(parts) < 4:
+        print("Invalid filename format:", filename, file=sys.stderr)
+        continue
+    version = parts[-4]
+    builddate = parts[-3]
+    buildtype = parts[-2]
+    device = parts[-1]
     print('hashing sha256 for {}'.format(filename), file=sys.stderr)
     sha256 = hashlib.sha256()
     for buf in iter(lambda : data.read(128 * 1024), b''):
